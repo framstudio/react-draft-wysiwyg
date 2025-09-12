ca59ce42363e86787546429ef36dd77b47f9d681
Mariusz Gałka <mariusz@framstudio.co.uk>
2025-09-12 13:38:29 +0200

    ARIA fixes

--- src/controls/ColorPicker/Component/index.js
+++ src/controls/ColorPicker/Component/index.js
@@ -1,130 +1,148 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
-import { stopPropagation } from '../../../utils/common';
-import Option from '../../../components/Option';
-import './styles.css';
+import { stopPropagation } from "../../../utils/common";
+import Option from "../../../components/Option";
+import "./styles.css";
 
 class LayoutComponent extends Component {
-  static propTypes = {
-    expanded: PropTypes.bool,
-    onExpandEvent: PropTypes.func,
-    onChange: PropTypes.func,
-    config: PropTypes.object,
-    currentState: PropTypes.object,
-    translations: PropTypes.object,
-  };
+    static propTypes = {
+        expanded: PropTypes.bool,
+        onExpandEvent: PropTypes.func,
+        onChange: PropTypes.func,
+        config: PropTypes.object,
+        currentState: PropTypes.object,
+        translations: PropTypes.object,
+    };
 
-  state = {
-    currentStyle: 'color',
-  };
+    state = {
+        currentStyle: "color",
+    };
 
-  componentDidUpdate(prevProps) {
-    const { expanded } = this.props;
-    if (expanded && !prevProps.expanded) {
-      this.setState({
-        currentStyle: 'color',
-      });
-    }
-  }
-
-  onChange = color => {
-    const { onChange } = this.props;
-    const { currentStyle } = this.state;
-    onChange(currentStyle, color);
-  };
-
-  setCurrentStyleColor = () => {
-    this.setState({
-      currentStyle: 'color',
-    });
-  };
-
-  setCurrentStyleBgcolor = () => {
-    this.setState({
-      currentStyle: 'bgcolor',
-    });
-  };
-
-  renderModal = () => {
-    const {
-      config: { popupClassName, colors },
-      currentState: { color, bgColor },
-      translations,
-    } = this.props;
-    const { currentStyle } = this.state;
-    const currentSelectedColor = currentStyle === 'color' ? color : bgColor;
-    return (
-      <div
-        className={classNames('rdw-colorpicker-modal', popupClassName)}
-        onClick={stopPropagation}
-      >
-        <span className="rdw-colorpicker-modal-header">
-          <span
-            className={classNames('rdw-colorpicker-modal-style-label', {
-              'rdw-colorpicker-modal-style-label-active':
-                currentStyle === 'color',
-            })}
-            onClick={this.setCurrentStyleColor}
-          >
-            {translations['components.controls.colorpicker.text']}
-          </span>
-          <span
-            className={classNames('rdw-colorpicker-modal-style-label', {
-              'rdw-colorpicker-modal-style-label-active':
-                currentStyle === 'bgcolor',
-            })}
-            onClick={this.setCurrentStyleBgcolor}
-          >
-            {translations['components.controls.colorpicker.background']}
-          </span>
-        </span>
-        <span className="rdw-colorpicker-modal-options">
-          {colors.map((c, index) => (
-            <Option
-              value={c}
-              key={index}
-              className="rdw-colorpicker-option"
-              activeClassName="rdw-colorpicker-option-active"
-              active={currentSelectedColor === c}
-              onClick={this.onChange}
-            >
-              <span
-                style={{ backgroundColor: c }}
-                className="rdw-colorpicker-cube"
-              />
-            </Option>
-          ))}
-        </span>
-      </div>
-    );
-  };
-
-  render() {
-    const {
-      config: { icon, className, title },
-      expanded,
-      onExpandEvent,
-      translations,
-    } = this.props;
-    return (
-      <div
-        className="rdw-colorpicker-wrapper"
-        aria-haspopup="true"
-        aria-expanded={expanded}
-        aria-label="rdw-color-picker"
-        title={
-          title || translations['components.controls.colorpicker.colorpicker']
+    componentDidUpdate(prevProps) {
+        const { expanded } = this.props;
+        if (expanded && !prevProps.expanded) {
+            this.setState({
+                currentStyle: "color",
+            });
         }
-      >
-        <Option onClick={onExpandEvent} className={classNames(className)}>
-          <img src={icon} alt="" />
-        </Option>
-        {expanded ? this.renderModal() : undefined}
-      </div>
-    );
-  }
+    }
+
+    onChange = (color) => {
+        const { onChange } = this.props;
+        const { currentStyle } = this.state;
+        onChange(currentStyle, color);
+    };
+
+    setCurrentStyleColor = () => {
+        this.setState({
+            currentStyle: "color",
+        });
+    };
+
+    setCurrentStyleBgcolor = () => {
+        this.setState({
+            currentStyle: "bgcolor",
+        });
+    };
+
+    renderModal = () => {
+        const {
+            config: { popupClassName, colors },
+            currentState: { color, bgColor },
+            translations,
+        } = this.props;
+        const { currentStyle } = this.state;
+        const currentSelectedColor = currentStyle === "color" ? color : bgColor;
+        return (
+            <div
+                className={classNames("rdw-colorpicker-modal", popupClassName)}
+                onClick={stopPropagation}
+                role="dialog"
+                aria-modal="true"
+                aria-label="Color picker dialog"
+            >
+                <div className="rdw-colorpicker-modal-header" role="tablist">
+                    <span
+                        className={classNames("rdw-colorpicker-modal-style-label", {
+                            "rdw-colorpicker-modal-style-label-active": currentStyle === "color",
+                        })}
+                        onClick={this.setCurrentStyleColor}
+                        role="tab"
+                        aria-selected={currentStyle === "color"}
+                        tabIndex={currentStyle === "color" ? 0 : -1}
+                    >
+                        {translations["components.controls.colorpicker.text"]}
+                    </span>
+                    <span
+                        className={classNames("rdw-colorpicker-modal-style-label", {
+                            "rdw-colorpicker-modal-style-label-active": currentStyle === "bgcolor",
+                        })}
+                        onClick={this.setCurrentStyleBgcolor}
+                        role="tab"
+                        aria-selected={currentStyle === "bgcolor"}
+                        tabIndex={currentStyle === "bgcolor" ? 0 : -1}
+                    >
+                        {translations["components.controls.colorpicker.background"]}
+                    </span>
+                </div>
+                <div
+                    className="rdw-colorpicker-modal-options"
+                    role="tabpanel"
+                    aria-label={`${currentStyle} color options`}
+                >
+                    {colors.map((c, index) => (
+                        <Option
+                            value={c}
+                            key={index}
+                            className="rdw-colorpicker-option"
+                            activeClassName="rdw-colorpicker-option-active"
+                            active={currentSelectedColor === c}
+                            onClick={this.onChange}
+                            title={`Select ${c} color`}
+                        >
+                            <span
+                                style={{ backgroundColor: c }}
+                                className="rdw-colorpicker-cube"
+                                aria-label={`Color ${c}`}
+                            />
+                        </Option>
+                    ))}
+                </div>
+            </div>
+        );
+    };
+
+    render() {
+        const {
+            config: { icon, className, title },
+            expanded,
+            onExpandEvent,
+            translations,
+        } = this.props;
+        return (
+            <div
+                className="rdw-colorpicker-wrapper"
+                aria-haspopup="dialog"
+                aria-expanded={expanded}
+                aria-label={
+                    translations["components.controls.colorpicker.colorpicker"] || "Color picker"
+                }
+                role="combobox"
+                title={title || translations["components.controls.colorpicker.colorpicker"]}
+            >
+                <Option
+                    onClick={onExpandEvent}
+                    className={classNames(className)}
+                    title={title || translations["components.controls.colorpicker.colorpicker"]}
+                >
+                    <img src={icon} alt="" />
+                </Option>
+                {expanded ? this.renderModal() : undefined}
+            </div>
+        );
+    }
 }
 
 export default LayoutComponent;
--- src/controls/Embedded/Component/index.js
+++ src/controls/Embedded/Component/index.js
@@ -1,155 +1,205 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
-import { stopPropagation } from '../../../utils/common';
-import Option from '../../../components/Option';
-import './styles.css';
+import { stopPropagation } from "../../../utils/common";
+import Option from "../../../components/Option";
+import "./styles.css";
 
 class LayoutComponent extends Component {
-  static propTypes = {
-    expanded: PropTypes.bool,
-    onExpandEvent: PropTypes.func,
-    onChange: PropTypes.func,
-    config: PropTypes.object,
-    translations: PropTypes.object,
-    doCollapse: PropTypes.func,
-  };
+    static propTypes = {
+        expanded: PropTypes.bool,
+        onExpandEvent: PropTypes.func,
+        onChange: PropTypes.func,
+        config: PropTypes.object,
+        translations: PropTypes.object,
+        doCollapse: PropTypes.func,
+    };
 
-  state = {
-    embeddedLink: '',
-    height: this.props.config.defaultSize.height,
-    width: this.props.config.defaultSize.width,
-  };
+    state = {
+        embeddedLink: "",
+        height: this.props.config.defaultSize.height,
+        width: this.props.config.defaultSize.width,
+    };
 
-  componentDidUpdate(prevProps) {
-    const { expanded, config } = this.props;
-    if (!expanded && prevProps.expanded) {
-      const { height, width } = config.defaultSize;
-      this.setState({
-        embeddedLink: '',
-        height,
-        width,
-      });
+    componentDidUpdate(prevProps) {
+        const { expanded, config } = this.props;
+        if (!expanded && prevProps.expanded) {
+            const { height, width } = config.defaultSize;
+            this.setState({
+                embeddedLink: "",
+                height,
+                width,
+            });
+        }
     }
-  }
 
-  onChange = () => {
-    const { onChange } = this.props;
-    const { embeddedLink, height, width } = this.state;
-    onChange(embeddedLink, height, width);
-  };
+    onChange = () => {
+        const { onChange } = this.props;
+        const { embeddedLink, height, width } = this.state;
+        onChange(embeddedLink, height, width);
+    };
 
-  updateValue = event => {
-    this.setState({
-      [`${event.target.name}`]: event.target.value,
-    });
-  };
+    updateValue = (event) => {
+        this.setState({
+            [`${event.target.name}`]: event.target.value,
+        });
+    };
 
-  rendeEmbeddedLinkModal() {
-    const { embeddedLink, height, width } = this.state;
-    const {
-      config: { popupClassName },
-      doCollapse,
-      translations,
-    } = this.props;
-    return (
-      <div
-        className={classNames('rdw-embedded-modal', popupClassName)}
-        onClick={stopPropagation}
-      >
-        <div className="rdw-embedded-modal-header">
-          <span className="rdw-embedded-modal-header-option">
-            {translations['components.controls.embedded.embeddedlink']}
-            <span className="rdw-embedded-modal-header-label" />
-          </span>
-        </div>
-        <div className="rdw-embedded-modal-link-section">
-          <span className="rdw-embedded-modal-link-input-wrapper">
-            <input
-              className="rdw-embedded-modal-link-input"
-              placeholder={
-                translations['components.controls.embedded.enterlink']
-              }
-              onChange={this.updateValue}
-              onBlur={this.updateValue}
-              value={embeddedLink}
-              name="embeddedLink"
-            />
-            <span className="rdw-image-mandatory-sign">*</span>
-          </span>
-          <div className="rdw-embedded-modal-size">
-            <span>
-              <input
-                onChange={this.updateValue}
-                onBlur={this.updateValue}
-                value={height}
-                name="height"
-                className="rdw-embedded-modal-size-input"
-                placeholder="Height"
-              />
-              <span className="rdw-image-mandatory-sign">*</span>
-            </span>
-            <span>
-              <input
-                onChange={this.updateValue}
-                onBlur={this.updateValue}
-                value={width}
-                name="width"
-                className="rdw-embedded-modal-size-input"
-                placeholder="Width"
-              />
-              <span className="rdw-image-mandatory-sign">*</span>
-            </span>
-          </div>
-        </div>
-        <span className="rdw-embedded-modal-btn-section">
-          <button
-            type="button"
-            className="rdw-embedded-modal-btn"
-            onClick={this.onChange}
-            disabled={!embeddedLink || !height || !width}
-          >
-            {translations['generic.add']}
-          </button>
-          <button
-            type="button"
-            className="rdw-embedded-modal-btn"
-            onClick={doCollapse}
-          >
-            {translations['generic.cancel']}
-          </button>
-        </span>
-      </div>
-    );
-  }
+    rendeEmbeddedLinkModal() {
+        const { embeddedLink, height, width } = this.state;
+        const {
+            config: { popupClassName },
+            doCollapse,
+            translations,
+        } = this.props;
+        return (
+            <div
+                className={classNames("rdw-embedded-modal", popupClassName)}
+                onClick={stopPropagation}
+                role="dialog"
+                aria-modal="true"
+                aria-label="Embedded content dialog"
+            >
+                <div className="rdw-embedded-modal-header">
+                    <span className="rdw-embedded-modal-header-option">
+                        {translations["components.controls.embedded.embeddedlink"]}
+                        <span className="rdw-embedded-modal-header-label" />
+                    </span>
+                </div>
+                <div className="rdw-embedded-modal-link-section">
+                    <span className="rdw-embedded-modal-link-input-wrapper">
+                        <label htmlFor="embedded-link-input" className="rdw-sr-only">
+                            {translations["components.controls.embedded.enterlink"]}
+                        </label>
+                        <input
+                            id="embedded-link-input"
+                            className="rdw-embedded-modal-link-input"
+                            placeholder={translations["components.controls.embedded.enterlink"]}
+                            onChange={this.updateValue}
+                            onBlur={this.updateValue}
+                            value={embeddedLink}
+                            name="embeddedLink"
+                            aria-required="true"
+                            aria-describedby="embedded-link-required"
+                        />
+                        <span
+                            id="embedded-link-required"
+                            className="rdw-image-mandatory-sign"
+                            aria-label="Required field"
+                        >
+                            *
+                        </span>
+                    </span>
+                    <div className="rdw-embedded-modal-size">
+                        <span>
+                            <label htmlFor="embedded-height-input" className="rdw-sr-only">
+                                Height
+                            </label>
+                            <input
+                                id="embedded-height-input"
+                                onChange={this.updateValue}
+                                onBlur={this.updateValue}
+                                value={height}
+                                name="height"
+                                className="rdw-embedded-modal-size-input"
+                                placeholder="Height"
+                                aria-required="true"
+                                aria-describedby="embedded-height-required"
+                            />
+                            <span
+                                id="embedded-height-required"
+                                className="rdw-image-mandatory-sign"
+                                aria-label="Required field"
+                            >
+                                *
+                            </span>
+                        </span>
+                        <span>
+                            <label htmlFor="embedded-width-input" className="rdw-sr-only">
+                                Width
+                            </label>
+                            <input
+                                id="embedded-width-input"
+                                onChange={this.updateValue}
+                                onBlur={this.updateValue}
+                                value={width}
+                                name="width"
+                                className="rdw-embedded-modal-size-input"
+                                placeholder="Width"
+                                aria-required="true"
+                                aria-describedby="embedded-width-required"
+                            />
+                            <span
+                                id="embedded-width-required"
+                                className="rdw-image-mandatory-sign"
+                                aria-label="Required field"
+                            >
+                                *
+                            </span>
+                        </span>
+                    </div>
+                </div>
+                <div
+                    className="rdw-embedded-modal-btn-section"
+                    role="group"
+                    aria-label="Dialog actions"
+                >
+                    <button
+                        type="button"
+                        className="rdw-embedded-modal-btn"
+                        onClick={this.onChange}
+                        disabled={!embeddedLink || !height || !width}
+                        aria-describedby={
+                            !embeddedLink || !height || !width
+                                ? "embedded-validation-message"
+                                : undefined
+                        }
+                    >
+                        {translations["generic.add"]}
+                    </button>
+                    <button type="button" className="rdw-embedded-modal-btn" onClick={doCollapse}>
+                        {translations["generic.cancel"]}
+                    </button>
+                    {(!embeddedLink || !height || !width) && (
+                        <div id="embedded-validation-message" className="rdw-sr-only">
+                            All fields are required to add embedded content
+                        </div>
+                    )}
+                </div>
+            </div>
+        );
+    }
 
-  render() {
-    const {
-      config: { icon, className, title },
-      expanded,
-      onExpandEvent,
-      translations,
-    } = this.props;
-    return (
-      <div
-        className="rdw-embedded-wrapper"
-        aria-haspopup="true"
-        aria-expanded={expanded}
-        aria-label="rdw-embedded-control"
-      >
-        <Option
-          className={classNames(className)}
-          value="unordered-list-item"
-          onClick={onExpandEvent}
-          title={title || translations['components.controls.embedded.embedded']}
-        >
-          <img src={icon} alt="" />
-        </Option>
-        {expanded ? this.rendeEmbeddedLinkModal() : undefined}
-      </div>
-    );
-  }
+    render() {
+        const {
+            config: { icon, className, title },
+            expanded,
+            onExpandEvent,
+            translations,
+        } = this.props;
+        return (
+            <div
+                className="rdw-embedded-wrapper"
+                aria-haspopup="dialog"
+                aria-expanded={expanded}
+                aria-label={
+                    translations["components.controls.embedded.embedded"] || "Embedded content"
+                }
+                role="combobox"
+            >
+                <Option
+                    className={classNames(className)}
+                    value="unordered-list-item"
+                    onClick={onExpandEvent}
+                    title={title || translations["components.controls.embedded.embedded"]}
+                >
+                    <img src={icon} alt="" />
+                </Option>
+                {expanded ? this.rendeEmbeddedLinkModal() : undefined}
+            </div>
+        );
+    }
 }
 
 export default LayoutComponent;
